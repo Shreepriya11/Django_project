26a12877bb8f6f9337e0a75e9318935f3d3375a2
Shreepriya11 <shreepriyasvrn@gmail.com>
2024-09-02 10:39:52 +0530

    first commit

---
+++ watchShop/homepage/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from . models import Watches,WatchesUploads,Wishlist,Cart,WatchReview,CartItem
+
+
+# Register your models here.
+admin.site.register(Watches)
+
+#WatchUploads
+class WatchUploadsAdmin(admin.ModelAdmin):
+    list_display=('name','description','price','image','count')
+    list_filter=('name','price')
+    search_fields=('name','description')
+    fields=['name','description','price','image','count']
+admin.site.register(WatchesUploads, WatchUploadsAdmin)
+
+#WatchList
+# class WishlistAdmin(admin.ModelAdmin):
+#     list_display=('user','products')
+#     list_filter=('user','products')
+# admin.site.register(Wishlist, WishlistAdmin)
+admin.site.register(Wishlist)
+
+
+admin.site.register(Cart)
+admin.site.register(CartItem)
+
+#watch Review
+class WatchReviewAdmin(admin.ModelAdmin):
+    list_display=('user','product','rating','review_text')
+    list_filter=('user','product','rating')
+admin.site.register(WatchReview, WatchReviewAdmin)
